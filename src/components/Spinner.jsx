731d7e2a684f6680dd407e133fbe3f83f925ff05
iqbalxrr <iamekbal7500@gmail.com>
2025-08-22 17:43:05 +0600

    fix spinner plm

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@
 
 export default function Spinner() {
   return (
-    <div className="flex justify-center items-center w-full h-full">
+    <div className="flex justify-center items-center min-h-screen w-full h-full">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
